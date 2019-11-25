8ebb1eaa581566dda8455c07401a7c915b456366
MainOlma <mainolma@gmail.com>
2019-11-25 05:12:25 +0300

    make shedule output with periods

--- src/CafeCard.jsx
+++ src/CafeCard.jsx
@@ -30,34 +30,23 @@ class CafeCard extends Component {
 
         const week=['Пн','Вт','Ср','Чт','Пт','Сб','Вс']
         const workTime = Array.isArray(info.workTime) ? info.workTime : JSON.parse(info.workTime)
-        const shedule = workTime.map((a,i)=>{return {day:i, time: a[0]}}).filter(el => el.time!=undefined)
-        const shorts =  nest()
+        const shedule = workTime.map((oneDay,i)=>{
+            const allPeriods = oneDay.map(timeParts => {
+                const timePeriod = timeParts.map(time => time.replace(/(\d?\d?):(\d?\d?)/gi, '$1<sup>$2</sup>'))
+                return timePeriod.join(' — ')  
+            })
+            return {day:i, time: allPeriods.join(', ')}
+        })
+            .filter(el => el.time!="")
+
+        const shortShedule =  nest()
             .key(d=>d.time)
             .rollup(els => {
                 if (els.length > 1)
-                    return week[ els[0].day ]+"-"+week[ els[els.length-1].day ] //Пн-чт —
+                    return week[ els[0].day ]+"-"+week[ els[els.length-1].day ] //Пн-чт
                 else return week[ els[0].day ]
             })
             .entries(shedule);
-
-        const short = shorts.map(el =>{
-
-                const keys = el.key.split(','),
-                    start = keys[0] || '',
-                    end = keys[1] || '',
-                    hours = start.split(':'),
-                    startH = hours[0] || '',
-                    startM = hours[1] || '',
-                    hours2 = end.split(":"),
-                    endH = hours2[0] || '',
-                    endM = hours2[1] || ''
-                return {
-                    key: '<span>' + startH + '<sup>' + startM + '</sup> — ' + endH + '<sup>' + endM + '</sup></span>',
-                    value: el.value
-                }
-
-        })
-
 
         return ReactDOM.createPortal(
             <div className="cafeCard cafeCard-absolute" style={{'background':info.color}}>
@@ -74,7 +63,7 @@ class CafeCard extends Component {
                     </div>
                 </div>
                 <div className='cafeCard--content--shedule'>
-                    {short.map((el, i) =>
+                    {shortShedule.map((el, i) =>
                         <div key={i}  className='cafeCard--content--shedule--row'>
                             <div>{el.value}</div>
                             <div dangerouslySetInnerHTML={{__html: el.key}} />
